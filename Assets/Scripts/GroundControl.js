fb78973c0a5b058bba60942610b85b54603feecb
vitorTest <vytorfelix2@gmail.com>
2017-10-07 21:24:35 -0300

    first commit

---
+++ Assets/Scripts/GroundControl.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var speed : float = 0.2f;
+var offset : float;
+
+function Start () {
+	
+}
+
+function Update () {
+	offset = Time.time * speed;
+	//renderer.material.mainTextureOffset = new Vector2(0, -offset);
+	GetComponent.<Renderer>().material.mainTextureOffset = new Vector2(0, -offset);
+}
